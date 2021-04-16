612f78c5f07a8922b2a0f9a6f531064233790296
Mark <geraldo.romero@ucsp.edu.pe>
2021-04-16 01:25:01 -0500

    a mimir :3

--- Manejo de String/18_27/main.cpp
+++ Manejo de String/18_27/main.cpp
@@ -1,92 +1,50 @@
 #include <iostream>
+#include <string>
 using namespace std;
-
-#include <iomanip>
-using std::setw;
-
-// prototipos
-void ordenamientoSeleccion( int [], const int, bool (*)( int, int ) );
-void intercambiar( int * const, int * const );
-bool ascendente( int, int ); // implementa el orden ascendente
-bool descendente( int, int ); // implementa el orden descendente
-
-int main(){
+void ordenamientoSeleccion( string trabajo[], const int tamanio, bool (*compara)( string, string ) )
+{
+    int menorOMayor;
+    int i;
+    for (i = 0; i < tamanio - 1; i++ )
+    {
+        menorOMayor = i;
+        int indc;
+        for (indc = i + 1; indc < tamanio; indc++ )
+            if ( !(*compara)( trabajo[ menorOMayor ], trabajo[ indc ] ) )
+                menorOMayor = indc;
+        trabajo[ menorOMayor ].swap( trabajo[ i ] );
+    }
+}
+bool ascendente( string a, string b )
+{
+    return a[0] < b[0];
+}
+bool descendente( string a, string b )
+{
+    return a[0] > b[0];
+}
+int main()
+{
     const int tamanioArreglo = 10;
-    int orden; // 1 = ascendente, 2 = descendente
-    int contador; // subíndice del arreglo
-    int a[ tamanioArreglo ] = { 2, 6, 4, 8, 10, 12, 89, 68, 45, 37 };
-
-    cout << "Escriba *1* para orden ascendente,\n"
-         << "Escriba *2* para orden descendente: ";
-    cin >> orden;
+    int disposicion;
+    int cont;
+    string a[ tamanioArreglo ] = {"gente", "humanidad", "humano", "persona", "hombre",
+                                  "mujer", "bebe", "adolescente", "adulto", "anciano"};
+    cout << "Escriba el 1 para orden ascendente,\n"
+         << "Escriba el 2 para orden descendente: ";
+    cin >> disposicion;
     cout << "\nElementos de datos en el orden original\n";
-// imprime el arreglo original
-    for ( contador = 0; contador < tamanioArreglo; contador++ )
-        cout << setw( 4 ) << a[ contador ];
-
-// ordena el arreglo en forma ascendente; pasa la función ascendente
-// como un argumento para especificar el orden ascendente
-    if ( orden == 1 )
-    {
-        ordenamientoSeleccion( a, tamanioArreglo, ascendente );
-        cout << "\nElementos de datos en orden ascendente\n";
-    } // fin de if
-
-        // ordena el arreglo en forma descendente; pasa la función descendente
-        // como argumento para especificar el orden en forma descendente
-    else
-    {
-        ordenamientoSeleccion( a, tamanioArreglo, descendente );
+    for ( cont = 0; cont < tamanioArreglo; cont++ )
+        cout << a[ cont ]<<" ";
+    cout<<endl;
+    if (disposicion != 1) {
+        ordenamientoSeleccion(a, tamanioArreglo, descendente);
         cout << "\nElementos de datos en orden descendente\n";
-    } // fin de la parte del if...else correspondiente al else
-
-    // imprime el arreglo ordenado
-    for ( contador = 0; contador < tamanioArreglo; contador++ )
-        cout << setw( 4 ) << a[ contador ];
-
-    cout << endl;
-    return 0; // indica que terminó correctamente
-} // fin de main
-
-// ordenamiento por selección multipropósito; el parámetro compara es
-// un apuntador a la función de comparación que determina el tipo de orden
-void ordenamientoSeleccion( int trabajo[], const int tamanio,
-                            bool (*compara)( int, int ) )
-{
-    int menorOMayor; // índice del elemento más pequeño (o grande)
-
-// itera a través de tamanio - 1 elements
-    for ( int i = 0; i < tamanio - 1; i++ )
-    {
-        menorOMayor = i; // primer índice para el vector remanente
-
-// itera para encontrar el índice del elemento más pequeño (o más grande)
-        for ( int index = i + 1; index < tamanio; index++ )
-            if ( !(*compara)( trabajo[ menorOMayor ], trabajo[ index ] ) )
-                menorOMayor = index;
-
-        intercambiar( &trabajo[ menorOMayor ], &trabajo[ i ] );
-    } // fin de if
-} // fin de la función ordenamientoSeleccion
-
-// intercambia los valores en las ubicaciones de memoria
-// a las que apuntan elemento1Ptr y elemento2Ptr1
-void intercambiar( int * const elemento1Ptr, int * const elemento2Ptr )
-{
-    int contenido = *elemento1Ptr;
-    *elemento1Ptr = *elemento2Ptr;
-    *elemento2Ptr = contenido;
-} // fin de la función intercambiar
-
-// determina si el elemento a es menor que
-// el elemento b para un orden ascendente
-bool ascendente( int a, int b )
-{
-    return a < b; // devuelve true si a es menor que b
-} // fin de la función ascendente
-
-// determina si el elemento a es mayor que
-// el elemento b para un orden descendente
-bool descendente( int a, int b ) {
-    return a > b; // devuelve true si
+    } else{
+        ordenamientoSeleccion(a, tamanioArreglo, ascendente);
+        cout << "\nElementos de datos en disposicion ascendente\n";
+    }
+    for(cont=0;cont<tamanioArreglo;cont++)
+        cout << a[ cont ]<<" ";
+    cout<<endl;
 }
